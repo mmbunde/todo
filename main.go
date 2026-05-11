4f786e1d507b56d458f32ed04ccd37bc78344f34
mmbunde <myles.bundegreen@gmail.com>
2026-05-11 12:31:27 -0600

    Add task completion support and improve empty list output

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	id := 1
 	var tasks []Task
 	var taskTitle string
-	message := "Would you like to add, list, delete, or quit a task?"
+	message := "Would you like to add, list, delete, or complete a task or quit?"
 loop:
 	for {
 		action := readInput(message)
@@ -24,18 +24,30 @@ loop:
 			tasks = append(tasks, Task{
 				ID:    id,
 				Title: taskTitle,
-				Done:  true,
+				Done:  false,
 			})
 			id++
 		case "list":
-			fmt.Printf("%-5s %-20s %s\n", "ID", "Title", "Done")
-			for _, task := range tasks {
-				check := "\u274c" //This is a red x
-				if task.Done == true {
-					check = "\u2705" //This is a green checkmark
+			if len(tasks) == 0 {
+				fmt.Println("No tasks are being tracked")
+			} else {
+				fmt.Printf("%-5s %-20s %s\n", "ID", "Title", "Done")
+				for _, task := range tasks {
+					check := "\u274c" //This is a red x
+					if task.Done == true {
+						check = "\u2705" //This is a green checkmark
+					}
+					fmt.Printf("%-5d %-20s %s\n", task.ID, task.Title, check)
 				}
-				fmt.Printf("%-5d %-20s %s\n", task.ID, task.Title, check)
 			}
+		case "complete":
+			taskTitle = readInput("Name of task completed: ")
+			for i, task := range tasks {
+				if task.Title == taskTitle {
+					tasks[i].Done = true
+				}
+			}
+
 		case "delete":
 			taskTitle = readInput("Name of the task to delete: ")
 			for i, task := range tasks {
@@ -46,7 +58,7 @@ loop:
 		case "quit", "exit":
 			break loop
 		default:
-			fmt.Println("Please select add, list or, delete")
+			fmt.Println("Please select add, list, delete, or quit")
 		}
 	}
 }
