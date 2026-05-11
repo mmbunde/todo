615fe0da1508c46ebab3ca4d77cb50d57d35492e
mmbunde <myles.bundegreen@gmail.com>
2026-05-10 20:10:26 -0600

    Extract user input handling into helper function

--- main.go
+++ main.go
@@ -8,40 +8,45 @@ import (
 	"strings"
 )
 
+var reader = bufio.NewReader(os.Stdin)
+
 func main() {
 	id := 1
 	var tasks []Task
 	var taskTitle string
-	reader := bufio.NewReader(os.Stdin)
 	message := "Would you like to add, list, or delete a task?"
 loop:
 	for {
-		fmt.Println(message)
-		taskTitle, _ = reader.ReadString('\n')
-		switch strings.TrimSpace(taskTitle) {
+		action := readInput(message)
+		switch action {
 		case "add":
-			fmt.Print("Name of the task: ")
-			taskTitle, _ = reader.ReadString('\n')
+			taskTitle = readInput("Name of the task: ")
 			tasks = append(tasks, Task{
 				ID:    id,
-				Title: strings.TrimSpace(taskTitle),
+				Title: taskTitle,
 				Done:  false,
 			})
 			id++
 		case "list":
 			fmt.Println("Tasks: ", tasks)
 		case "delete":
-			fmt.Print("Name of the task to delete: ")
-			taskTitle, _ = reader.ReadString('\n')
+			taskTitle = readInput("Name of the task to delete: ")
 			for i, task := range tasks {
-				if task.Title == strings.TrimSpace(taskTitle) {
+				if task.Title == taskTitle {
 					tasks = slices.Delete(tasks, i, i+1)
 				}
 			}
 		case "quit", "exit":
 			break loop
 		default:
-			fmt.Println("Unknown error")
+			fmt.Println("Please select add, list or, delete")
 		}
 	}
+}
+
+// Helper functions to simplify the code
+func readInput(prompt string) string {
+	fmt.Println(prompt)
+	userInput, _ := reader.ReadString('\n')
+	return strings.TrimSpace(userInput)
 }
